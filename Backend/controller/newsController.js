f6e79e8d4dd3cb582b9a0701fdfb0438efd1ff2d
Yashgupta365 <87028683+Yashgupta365@users.noreply.github.com>
2022-02-20 20:14:25 +0530

    Delete Backend directory

--- Backend/controller/newsController.js
+++
@@ -1,6 +0,0 @@
-const News = require("../models/newsModel");
-const factory = require("./handlerFactory");
-
-exports.getAllTours = factory.getAll(News);
-// exports.getTour = factory.getOne(Tour, { path: "reviews" });
-exports.createTour = factory.createOne(News);
